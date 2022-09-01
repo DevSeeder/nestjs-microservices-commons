8482d732b75882cbaa2e6ee587c661d5ea57d6a9
Maick Speck <maicksantos05@hotmail.com>
2022-09-01 09:35:05 -0300

    1.0.4

--- src/model/dto.model.ts
+++ src/model/dto.model.ts
@@ -3,7 +3,7 @@ import { EmptyPropException } from '@devseeder/microservices-exceptions';
 export abstract class DTO {
   static validateIsAnyEmptyKey(context: Object) {
     Object.keys(context).forEach(function (key) {
-      if (context[key].length === 0) throw new Error(key);
+      if (context[key].length === 0) throw new EmptyPropException(key);
     });
   }
 }
