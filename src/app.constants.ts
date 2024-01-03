1b9d749157696532904e89cbb165d83242dd8f3b
Maick <maicksantos05@hotmail.com>
2024-01-03 14:35:55 -0300

    22

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,17 +1,3 @@
-import { SearchEgineOperators } from '@devseeder/nestjs-microservices-schemas';
-
-// OPERATORS ENUMS
-export const SKIP_ENUMS_ALIAS = [SearchEgineOperators.IN];
-export const SKIP_ENUMS = [
-  SearchEgineOperators.BETWEEN,
-  SearchEgineOperators.IN,
-];
-export const VALIDATE_ID_ENUMS = [
-  SearchEgineOperators.IN,
-  SearchEgineOperators.NOT_IN,
-  SearchEgineOperators.NOT_EQUAL,
-];
-
 export enum DependencyInjectorToken {
   SCOPE_KEY = 'SCOPE_KEY',
 }
